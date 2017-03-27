b9d74fc7e6b846354e5716e67c31d2c958b11a36
sudolawyer <stephansdalal@gmail.com>
2017-03-27 15:09:17 -0400

    Update sr34.py

--- sr34.py
+++ sr34.py
@@ -8,14 +8,16 @@ import sys
 import re
 import os
 
-print(os.getcwd())
-text = open("sres34.txt").read()
+print(os.getcwd()) # prints working directory 
+text = open("sres34.txt").read() # opens our text file so we know who voted for what
 #print(text)
 
+# Let's do a quick count of the total votes SR 34 received
 alltext = re.findall("[A-Z]-",text)
 allTextTotal = len(alltext)
 print("TOTAL SENATORS IN THE VOTE: ", len(alltext))
 
+# Now let's split the count of votes by party
 republicans = re.findall("R-",text)
 print("REPUBLICANS: ", len(republicans))
 
@@ -25,6 +27,7 @@ print("DEMOCRATS: ", len(democrats))
 others = re.findall("I-",text)
 print("INDEPENDENTS: ", len(others))
 
+# Let's do some verification to make sure we didn't miss any votes
 total = len(democrats) + len(republicans) + len(others)
 print("TOTAL: ", total)
 
@@ -33,7 +36,9 @@ if total == allTextTotal:
 else:
     print("ERROR: TOTALS DO NOT MATCH")
 
-textRows = text.split('\n')
+# Now let's clean the data up so we can do something cool with it 
+# I want to isolate SENATOR, PARTY, STATE, VOTE and store that data as csv
+textRows = text.split('\n') 
 
 for each in textRows:
     new = each.split('(')
