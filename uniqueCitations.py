906139dcba86ce2261b3aa9f5ea872d161211838
Stephan Dalal <stephansdalal@gmail.com>
2017-03-28 20:25:21 -0400

    Create uniqueCitations.py

---
+++ uniqueCitations.py
@@ -0,0 +1,27 @@
+```Python
+# This script will scan a plain text document looking for legal citations in the document.
+# It will then print all of the unique citations, line by line, as well as the total number
+# of unique citations used in craft the opinion.  For example, the Chevron opinion relies on
+# 50 unique cases.
+
+#Created by: Stephan S. Dalal
+#Affiliation: Georgetown Law
+#Language: Python3
+#Date: March 2017
+
+import re
+
+opinionString = open("chevron.txt", encoding="utf-8", errors="ignore").read() #some unicode error magic
+
+def opinionCitation(opinionString):
+
+    citationRegex = '\d{1,3} U.S. \d{1,3}'
+    match = re.findall(citationRegex, opinionString)
+    print(match)
+    uniqueMatches=len(match)
+    for each in match:
+        matches = each.split('\n')
+        print(matches)
+    print("TOTAL UNIQUE CITATIONS: ", uniqueMatches)
+    
+opinionCitation(opinionString)
